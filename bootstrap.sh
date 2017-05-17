eabd4db4290f13e1b818e14697ca06798def5b46
Mac Radigan <na>
2017-05-16 23:00:47 -0700

    added boostrap script for Ubuntu

---
+++ bootstrap.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+## boostrap.sh (for Mrs. Watson)
+## Mac Radigan
+
+ #sudo apt-get install -y \
+ #  libappindicator1      \
+ #  libindicator7         \
+ #  libc6                 \
+ #  libc6-dev
+  git submodule init && git submodule update --recursive --remote
+  cmake .
+  make -j
+
+## *EOF*
